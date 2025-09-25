9041f86c9e734e3d570153c4f3cddd93f6e47de1
jimenaduque07-jpg <jimenaduque07@gmail.com>
2025-09-24 22:09:53 -0500

    controller y collider

--- Assets/scripts/controller.cs
+++ Assets/scripts/controller.cs
@@ -1,18 +1,48 @@
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class scripts : MonoBehaviour
-    
+
 {
     public float speed = 5f;
+    public int score = 0;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // vamos a leer las teclas wasd o las flechas del teclado
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //creamos un vector para almacenar la direccion del movimiento
+        Vector3 direction = new Vector3(moveHorizontal, moveVertical, 0);
+
+        transform.Translate(direction * speed * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+
+    {
+        if (other.CompareTag("collectable"))
+        {
+            score = score + 1;
+
+
+            Destroy(other.gameObject);
+            Debug.Log("collected!!!");
+            Debug.Log("score: " + score);
+
+        }
+
+
     }
 }
